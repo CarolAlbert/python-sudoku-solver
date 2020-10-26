7a15103c283c13ae86c85b34cd7a411e03960e32
legerity <68836088+legerity@users.noreply.github.com>
2020-10-26 15:05:24 +0530

    Add files via upload

---
+++ sudoku/psued.py
@@ -0,0 +1,12 @@
+# back tracking algorithm
+# pick space sqaure
+# try all numbers
+# finding one that works(yes/no)
+# go to next space square
+# repeat
+# if error:
+# backtrack
+# go to previous step
+# repeat process, if it still doesn't work backtrack again.
+# else no error:
+# go to next sqaure
